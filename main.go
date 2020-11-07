8230624a42bf568aa8c2b0d0ae00c05e3ff9cc63
TOGEP <sg.endo0302@gmail.com>
2020-11-07 14:38:14 +0900

    change subcommands name

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main(){
-  answerAction := flag.NewFlagSet("answer", flag.ExitOnError)
+  runAction := flag.NewFlagSet("run", flag.ExitOnError)
   listAction := flag.NewFlagSet("list", flag.ContinueOnError)
   addAction := flag.NewFlagSet("add", flag.ExitOnError)
   addProblem := addAction.String("problem", "", "problem")
@@ -21,8 +21,8 @@ func main(){
   }
 
   switch os.Args[1] {
-    case "answer":
-      answerAction.Parse(os.Args[2:])
+    case "run":
+      runAction.Parse(os.Args[2:])
 
     case "list":
       listAction.Parse(os.Args[2:])
